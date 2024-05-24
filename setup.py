3a525303ba7da881bb4adc58ae4bcfa83971467a
kwonmoyang <167264151+kwonmoyang@users.noreply.github.com>
2024-05-24 14:53:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import time
 import os
 
-setup(name='kwonmo',
+setup(name='o9common',
       packages=['o9dummyfolder'],
       include_package_data=True
       )
